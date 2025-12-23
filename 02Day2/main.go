5d22ec139820ee088789560fc5612277f76eee11
Kiran Gautam <gautamkiran878@gmail.com>
2025-12-23 22:07:32 +0545

    learn:Learnt about formatted Printf in go

---
+++ 02Day2/main.go
@@ -0,0 +1,14 @@
+package main 
+
+import "fmt"
+
+var conference = "Go Conference";
+var tickets = 100;
+var remainingTickets = 100;
+
+
+func main(){
+	fmt.Printf("Welcome to %v\n", conference)
+	fmt.Printf("We have  total %v tickets\n", tickets)
+	fmt.Printf("We have  total %v tickets\n", remainingTickets)
+}
