b5815f8f2f46dd4f516e6591b81887b2fed83271
Kiran Gautam <gautamkiran878@gmail.com>
2026-01-06 10:14:57 +0545

    feat: add initial 'Hello, World!' Go program and project IDE configuration

--- 01hello/main.go
+++ 01hello/main.go
@@ -3,8 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(`Hello, World!`)
 	fmt.Println("Welcome to Go programming!")
 	fmt.Println("This is a simple Go application.")
-	fmt.Println("THis is demo from idefrom jetbrains ")
+	fmt.Println("This is demo from IDE from JetBrains ")
+	
+	// Demonstrate the addition function
+	demonstrateAddition()
+}
+
+//function to make a addition
+func add(a, b int) int {
+	return a + b
+}
+
+func demonstrateAddition() {
+	result := add(5, 3)
+	fmt.Printf("Addition of 5 and 3 is: %d\n", result)
 }
